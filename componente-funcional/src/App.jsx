6356090f2f2d8794b71795ad522c27bf4e2d916b
natalysodre <natalyssodre@gmail.com>
2024-02-28 13:35:17 -0300

    primeiro comentário

---
+++ componente-funcional/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Componente from "./components/Componente.jsx"
+
+function App() {
+  return (
+    <>
+      <Componente/>
+    </>
+  )
+}
+
+export default App
---
+++ componente-funcional/src/components/Componente.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Componente.css"
+
+function Componente(){
+    return(
+        <section>
+            <div className="componenteFuncional">
+                <h1>Olá, eu sou o Componente Funcional!</h1>
+                <p>Neste desafio, foi criado um componente funcional simples para a prática das habilidades em desenvolvimento web. Utilizou-se o Vite como ferramenta de construção e desenvolvimento, aproveitando sua eficiência e simplicidade.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Componente
